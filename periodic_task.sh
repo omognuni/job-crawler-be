3a1def0d52a38540619d127afc3671519c744a0b
yongwun <yongwun@adenasoft.com>
2025-10-28 14:58:15 +0900

    Fix task script

--- periodic_task.sh
+++ periodic_task.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker exec -it app sh -c 'uv run python run_agent.py'
+# 0 0 * * * ${pwd}/periodic_task.sh >> /home/ubuntu/cron.log 2>&1
+docker exec -i app sh -c 'uv run python run_agent.py'
